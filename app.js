d1068e67daffe65c4d8179648783b42270d8b0a0
jjerry1225 <i183801225@gmail.com>
2023-01-03 16:06:05 +0800

    feat: separate routes based on SoC principle

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const exphbs = require("express-handlebars")
 const app = express()
 const port = 3000
 
+// 引用路由器，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案。
+const routes = require("./routes")
+
 // 套用express-handlebars的模板引擎
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 
 // 設定route
-app.get("/", (req, res) => {
-  res.render("index")
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
