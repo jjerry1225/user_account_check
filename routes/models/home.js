b4967292b9cb9de743c694b9cbdac098767ed063
jjerry1225 <i183801225@gmail.com>
2023-01-04 12:02:57 +0800

    feat: add route for check user account and add success.handlebars for login successfully

--- routes/models/home.js
+++ routes/models/home.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const router = express.Router()
+const users = require("../../user.json")
 
 router.get("/", (req, res) => {
   res.render("index")
 })
 
 router.post("/", (req, res) => {
-  console.log(req.body)
+  const InputEmail = req.body.InputEmail
+  const InputPassword = req.body.InputPassword
+  const index = users.findIndex(user => user.email === InputEmail && user.password === InputPassword)
+  
+  if (index === -1) {
+    res.render("index", {index})
+  } else {
+    res.render("success", {firstName: users[index].firstName})
+  }
 })
 
 module.exports = router
