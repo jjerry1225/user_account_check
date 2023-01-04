ff9ff0a1fe9a376492fb356c3862377b9a9088e7
jjerry1225 <i183801225@gmail.com>
2023-01-04 15:54:18 +0800

    feat: adjust coding style on home.js

--- routes/models/home.js
+++ routes/models/home.js
@@ -1,21 +1,22 @@
-const express = require("express")
-const router = express.Router()
-const users = require("../../user.json")
+const express = require("express");
+const router = express.Router();
+const users = require("../../user.json");
 
 router.get("/", (req, res) => {
-  res.render("index")
-})
+  res.render("index");
+});
 
 router.post("/", (req, res) => {
-  const InputEmail = req.body.InputEmail
-  const InputPassword = req.body.InputPassword
-  const index = users.findIndex(user => user.email === InputEmail && user.password === InputPassword)
-  
-  if (index === -1) {
-    res.render("index", {index})
-  } else {
-    res.render("success", {firstName: users[index].firstName})
-  }
-})
+  const { InputEmail, InputPassword } = req.body;
+  const index = users.findIndex(
+    (user) => user.email === InputEmail && user.password === InputPassword
+  );
 
-module.exports = router
+  if (index === -1) {
+    res.render("index", { index });
+  } else {
+    res.render("success", { firstName: users[index].firstName });
+  }
+});
+
+module.exports = router;
