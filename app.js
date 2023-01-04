a740395d62c6aaa3daa35017b360baed03f210fb
jjerry1225 <i183801225@gmail.com>
2023-01-04 10:27:24 +0800

    feat: adjust views by bootstrap

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const app = express()
-const port = 3000
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
 
 // 引用路由器，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案。
-const routes = require("./routes")
+const routes = require("./routes");
 
 // 套用express-handlebars的模板引擎
-app.engine("handlebars", exphbs({ defaultLayout: "main" }))
-app.set("view engine", "handlebars")
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // 設定route
-app.use(routes)
+app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
-})
+  console.log(`Express is listening on http://localhost:${port}`);
+});
